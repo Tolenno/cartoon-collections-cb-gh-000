73114ee799aa98978353eee81f6a5c56edfad51f
Matt Hough <matt@hough.tv>
2017-09-26 22:41:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
   end
 end
 
-array = ["banana", "cheddar", "sock"]
+array = ["cheddar", "cheddar", "sock"]
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
