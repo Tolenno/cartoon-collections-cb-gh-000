a80a7bc24e4990f69d06d47842a7df7212d73b72
Matt Hough <matt@hough.tv>
2017-09-26 23:16:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,7 @@ array = ["banana", "cheddar", "sock"]
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect.with_index do |maybe_cheese, index|
-    maybe_cheese == cheese_types[index]
-    binding.pry
+  array.detect.with_index do |maybe_cheese|
+
   end
 end
