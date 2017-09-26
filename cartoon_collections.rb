248c67e6884681153cba28a4f218767a6eeda3ec
Matt Hough <matt@hough.tv>
2017-09-26 22:34:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,8 @@ def long_planeteer_calls(array)
   end
 end
 
+array = ["banana", "cheddar", "sock"]
+
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.detect.with_index do |maybe_cheese, index|
