e194932e8d70b26b9e4cd7219c56069e430623c5
Matt Hough <matt@hough.tv>
2017-09-26 22:27:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect(cheese_types) #.with_index do |maybe_cheese, index|
+  array.include?(cheese_types) #.with_index do |maybe_cheese, index|
     #maybe_cheese == cheese_types[index]
 end
