8dca4f9598cbf6ce7ef1363aa4d64646b7dc948e
Matt Hough <matt@hough.tv>
2017-09-26 22:03:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(array)
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map.with_index do |calls|
+    calls = "#{calls}!"
+    calls.capitalize
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.include? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
